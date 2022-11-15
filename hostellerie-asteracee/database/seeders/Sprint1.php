2eb38cbf607c2879bec6163f850269fa14227ec9
Gaetan-CRISTINA <gaetan.cristina@gmail.com>
2022-11-15 15:57:42 +0100

    Seeder Sprint1

---
+++ hostellerie-asteracee/database/seeders/Sprint1.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+class Sprint1 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            Medias::class,
+            Infos::class,
+            Contacts::class,
+            Heroes::class,
+            Reviews::class,
+            Rooms::class,
+            Services::class,
+            Users::class
+        ]);
+
+    }
+}
