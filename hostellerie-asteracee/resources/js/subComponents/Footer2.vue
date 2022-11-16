0748d9c0c94ccdd0a8a8979fe8c0350b6eb5dadd
Gaetan-CRISTINA <gaetan.cristina@gmail.com>
2022-11-16 11:10:02 +0100

    update gitignore

---
+++ hostellerie-asteracee/resources/js/subComponents/Footer2.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="flex justify-between sm:items-center sm:flex-col md:flex-col lg:flex-row bg-black text-white">
+        <div class="section1">
+            <div class="flex flex-col">
+                <img class="w-6" src="../assets/images/icones/location.svg" alt="icone de localisation">
+                <p>25 quai du lac - 74 000 Annecy</p>
+            </div>
+
+            <div class="siteMapp">
+                <h5>Plan du site</h5>
+                <a href="*">Accueil</a>
+                <a href="*">Nos chambres</a>
+                <a href="*">Nos Services</a>
+                <a href="*">Nos Nouveautés</a>
+            </div>
+        </div>
+
+        <div class="section2">
+            <div class="phone">
+                <img class="w-6" src="../assets/images/icones/phone.svg" alt="icone de téléphone">
+                <p>+334 56 56 56 56</p>
+            </div>
+
+            <img class="rosace" src="../assets/images/Frame231.svg" alt="décoration">
+        </div>
+
+        <div class="section3">
+            <div class="email">
+                 <img class="w-6" src="../assets/images/icones/email.svg" alt="icone de mail">
+                <p>email@email.fr</p>
+            </div>
+            <div class="socialNetwork">
+                <img src="../assets/images/icones/facebook.svg" alt="facebook">
+                <img src="../assets/images/icones/instagram.svg" alt="instagram">
+                <img src="../assets/images/icones/twitter.svg" alt="twitter">
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Footer2'
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Radley&display=swap');
+
+
+</style>
