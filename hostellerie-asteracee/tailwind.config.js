6aedc01c7661eb044f39b010e08e98c2d7ee33ea
Zakaria <zakaria.aaddi@gmail.com>
2022-11-17 16:59:03 +0100

    Deals Banner Content OK

--- hostellerie-asteracee/tailwind.config.js
+++ hostellerie-asteracee/tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
     './resources/**/*.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      height:{
+        '147': '147px',
+        '330': '330px',
+        '380': '380px',
+        '450': '450px'
+      },
+    },
   },
   plugins: [],
 }
