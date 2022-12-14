3327d5a2dcf5cc2c1ae0b0f376b593911386ceee
Gaetan-CRISTINA <gaetan.cristina@gmail.com>
2022-12-14 15:17:30 +0100

    hotfix again

--- hostellerie-asteracee/resources/js/views/Connexion.vue
+++ hostellerie-asteracee/resources/js/views/Connexion.vue
@@ -6,19 +6,19 @@
         <hr class="my-2 h-px border-1 border-[#E6B34B] w-1/4">
         <hr class="my-0 h-px border-2 border-[#E6B34B] w-1/3 mb-8">
         <div class="flex flex-col lg:flex-row lg:w-2/3 justify-center items-center border-t-4 border-t-[#E6B34B] bg-[#272023] text-white">
-            <form class="flex flex-col p-8 pb-8 lg:w-1/2">
+            <form @submit.prevent="connectUser" class="flex flex-col p-8 pb-8 lg:w-1/2">
             <p class="text-center">Déjà Client ?</p>
                     <label for="email" class="lg:w-1/2 lg:mx-auto">Email</label>
-                    <input type="email" name="email" v-model="credential.email" class="lg:w-1/2 lg:mx-auto">
+                    <input type="email" name="email" v-model="email" class="lg:w-1/2 lg:mx-auto">
                      <div class="error" v-if="emailEmpty">
                         Vous devez saisir un mail
                     </div>
                     <label for="password" class="lg:w-1/2 lg:mx-auto">Password</label>
-                    <input type="password" name="password" v-model="credential.password" class="lg:w-1/2 lg:mx-auto">
+                    <input type="password" name="password" v-model="password" class="lg:w-1/2 lg:mx-auto">
                      <div class="error" v-if="passwordEmpty">
                      Merci de saisir votre mot de passe
                     </div>
-                    <button @click="login(credential)" class="text-[#272023] my-8 lg:w-1/2 lg:mx-auto">Se connecter</button>
+                    <button class="text-[#272023] my-8 lg:w-1/2 lg:mx-auto">Se connecter</button>
                     <div class="error" v-if="matching">
                     Une erreure est survenue
                     </div>
@@ -26,7 +26,7 @@
             <div class=" flex flex-col justify-center items-center w-1/2 border-t-2 lg:border-l-2 lg:border-l-[#E6B34B] lg:border-t-0 border-t-[#E6B34B] lg:w-1/2">
                 <p>Nouveau Client ?</p>
                  <router-link :to="{ name: 'CreateUser' }" class="text-white mx-auto my-8">
-                    <button  class="text-[#272023] w-full">Créer un compte</button>
+                    <button class="text-[#272023] w-full">Créer un compte</button>
                 </router-link>
             </div>
         </div>
@@ -34,25 +34,47 @@
 </template>
 
 <script>
-
-import { useAuthStore } from '../store/authentificationStore';
-import { mapActions } from 'pinia';
+import axios from 'axios'
+import localStorage from '../services/localStorageProvider'
+import store from '../store/index.js'
 export default {
     name: 'Connexion',
     components:{
     },
     data()  {
         return {
-            credential:{
-                email : this.email,
-                password : this.password,
-            },
-            email: '',
-            password:''
+            email : "",
+            password : "",
+            emailEmpty: false,
+            passwordEmpty: false,
+            matching: false
         }
     },
     methods:{
-        ...mapActions(useAuthStore,['login'])
+        async connectUser(){
+            if(this.email == ""){
+                this.emailEmpty = true;
+            }
+            if(this.password == ""){
+                this.passwordEmpty = true;
+            }
+            if(!this.emailEmpty && !this.passwordEmpty){
+                this.datas = {
+                email: this.email,
+                password: this.password
+                };
+                this.response = await axios.post('http://127.0.0.1:8000/api/login', this.datas);
+                store.commit('addUser',{
+                    email: this.email,
+                    token: this.response.data.token
+                });
+                localStorage.set("user", [this.email,this.response.data.token]);
+                    this.$router.push({ name: 'Home'});
+                }else{
+                    this.matching = true;
+                }
+
+            }
     }
 }
 
